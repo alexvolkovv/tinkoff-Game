85d88ffc18ef255dfbb7fe7a8fd19eeaed20ce54
Aleksandr Volkov3 <aleksandr_volkov3@epam.com>
2022-07-08 12:32:09 +0300

    setup react project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import AppRouter from './components/AppRouter'
+
+export function App() {
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  )
+}
